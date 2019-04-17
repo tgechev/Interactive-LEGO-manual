893b0a3b9c6808bd3b8ba5e2b1b4dc9e076b0c53
GeorgiNedelev <george.nedelev.gmail.com>
2019-04-17 11:35:59 +0200

    pushed Georgi's code in Lego-manual/Source.cpp

--- Lego_manual/Source.cpp
+++ Lego_manual/Source.cpp
@@ -1,5 +1,5 @@
 #include "Camera.h"
-
+#include<time.h>
 using namespace std;
 using namespace cv;
 
@@ -7,39 +7,58 @@ const int max_value_H = 360 / 2;
 const int max_value = 255;
 const String window_capture_name = "Video Capture";
 const String window_detection_name = "Object Detection";
+const String difference = "difference";
 int low_H = 0, low_S = 0, low_V = 0;
 int high_H = max_value_H, high_S = max_value, high_V = max_value;
 
-static void on_low_H_thresh_trackbar(int, void *)
-{
-	low_H = min(high_H - 1, low_H);
-	setTrackbarPos("Low H", window_detection_name, low_H);
-}
-static void on_high_H_thresh_trackbar(int, void *)
-{
-	high_H = max(high_H, low_H + 1);
-	setTrackbarPos("High H", window_detection_name, high_H);
-}
-static void on_low_S_thresh_trackbar(int, void *)
-{
-	low_S = min(high_S - 1, low_S);
-	setTrackbarPos("Low S", window_detection_name, low_S);
-}
-static void on_high_S_thresh_trackbar(int, void *)
-{
-	high_S = max(high_S, low_S + 1);
-	setTrackbarPos("High S", window_detection_name, high_S);
-}
-static void on_low_V_thresh_trackbar(int, void *)
-{
-	low_V = min(high_V - 1, low_V);
-	setTrackbarPos("Low V", window_detection_name, low_V);
-}
-static void on_high_V_thresh_trackbar(int, void *)
-{
-	high_V = max(high_V, low_V + 1);
-	setTrackbarPos("High V", window_detection_name, high_V);
-}
+
+int thresh = 255;
+int max_thresh = 255;
+RNG rng(12345);
+Mat image, image_gray;
+int largest_area = 0;
+int largest_contour_index = 0;
+Point area0;
+Rect bounding_rect;
+
+
+int low_thresh = 89;
+int max_thresh1 = 255;
+
+Mat camFrame, frame_HSV, frame_threshold;
+bool urMom = true;
+bool urMom1 = true;
+Mat newFrame, Difference, original;
+const char* thresh_window = "Thresh";
+int threshold_value = 0;
+int threshold_type = 3;
+//int const max_value = 255;
+int const max_type = 4;
+int const max_BINARY_value = 255;
+
+Mat src, src_gray, dst, roi;
+char* window_name = "Threshold Demo";
+
+		/// used in color set
+
+int minHue, maxHue, minSat, maxSat, minValue, maxValue;
+int minHueRed, maxHueRed, minSatRed, maxSatRed, minValueRed, maxValueRed;
+int minHueBlue, maxHueBlue, minSatBlue, maxSatBlue, minValueBlue, maxValueBlue;
+int minHueGreen, maxHueGreen, minSatGreen, maxSatGreen, minValueGreen, maxValueGreen;
+
+
+int margin = 15; // used to set a margin of hsv
+int switchColor = NULL;// used in color set swtich statement
+bool colorFound = false; // used to detect certain colors only when a color is defined for inrange();
+
+time_t timer; // not used yet
+Mat thresh_callback(Mat input);
+Mat colorExtraction(Mat input);
+Mat Threshold_Demo(Mat input);
+void checkForHand(Mat frame);
+void color1(Mat image);
+
+//void HSVtoRGB(int H, double S, double V, int output[3]);
 
 int main() {
 
@@ -49,60 +68,453 @@ int main() {
 
 	cam->SetROI(0, 0, 1152, 964);
 
-	Mat camFrame, frame_HSV, frame_threshold;
+	roi = cam->getFrame();
 
-	Mat canny_output,gray;
-	vector<vector<Point> > contours;
-	vector<Vec4i> hierarchy;
+	checkForHand(camFrame);
+
+	urMom = false;
+	urMom1 = false;
 
 	namedWindow(window_capture_name, WINDOW_NORMAL);
 	namedWindow(window_detection_name, WINDOW_NORMAL);
-
-	// Trackbars to set thresholds for HSV values
-	createTrackbar("Low H", window_detection_name, &low_H, max_value_H, on_low_H_thresh_trackbar);
-	createTrackbar("High H", window_detection_name, &high_H, max_value_H, on_high_H_thresh_trackbar);
-	createTrackbar("Low S", window_detection_name, &low_S, max_value, on_low_S_thresh_trackbar);
-	createTrackbar("High S", window_detection_name, &high_S, max_value, on_high_S_thresh_trackbar);
-	createTrackbar("Low V", window_detection_name, &low_V, max_value, on_low_V_thresh_trackbar);
-	createTrackbar("High V", window_detection_name, &high_V, max_value, on_high_V_thresh_trackbar);
-
-	/*createButton(whiteBtn, radioSelect, (void*)&whiteBtn, QT_RADIOBOX);
-	createButton(blackBtn, radioSelect, (void*)&blackBtn, QT_RADIOBOX);
-	createButton("button5", callbackButton1, NULL, QT_RADIOBOX);
-	createButton("button5", callbackButton1, NULL, QT_RADIOBOX);
-	createButton("button5", callbackButton1, NULL, QT_RADIOBOX);
-	createButton("button5", callbackButton1, NULL, QT_RADIOBOX);
-	createButton("button5", callbackButton1, NULL, QT_RADIOBOX);
-	createButton("button5", callbackButton1, NULL, QT_RADIOBOX);
-	createButton("button5", callbackButton1, NULL, QT_RADIOBOX);
-	createButton("button5", callbackButton1, NULL, QT_RADIOBOX);*/
+	namedWindow(difference, WINDOW_AUTOSIZE);	
 
 	while (true) {
 
 		camFrame = cam->getFrame();
 
-		flip(camFrame, camFrame, -1);
+		//flip(camFrame, camFrame, -1);
 
-		// Convert from BGR to HSV colorspace
-		cvtColor(camFrame, frame_HSV, COLOR_BGR2HSV);
-		// Detect the object based on HSV Range Values
-		inRange(frame_HSV, Scalar(low_H, low_S, low_V), Scalar(high_H, high_S, high_V), frame_threshold);
-		// Show the frames
-		imshow(window_capture_name, camFrame);
-		imshow(window_detection_name, frame_threshold);
+		if (urMom) {
 
+			original = camFrame;
 
-		//Canny(gray, canny_output, 50, 150, 3);
-		//findContours(canny_output, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE, Point(0, 0));
+			imshow(window_capture_name, camFrame);
 		
+			urMom = false;
+		}
+
+		if (urMom1) {
+			newFrame = camFrame;
+			
+
+			
+			absdiff(Threshold_Demo(original), Threshold_Demo(newFrame), Difference);
+			
+			imshow(difference, Difference);
+			thresh_callback(Difference);
+
+			urMom1 = false;
+		}
 
 
-		if (waitKey(30) >= 0)
+		if (urMom1 == false && urMom == false && colorFound == true) {
+			Mat source = camFrame;
+
+			colorExtraction(source);
+			thresh_callback(source);
+
+			colorFound = false;
+
+		}
+		
+		int keyCode = waitKey(30);
+
+
+		if (keyCode == 'v') {
+			urMom1 = true;
+		}
+
+
+		if (keyCode == 'b') {
+			urMom = true;
+		}
+		else if(keyCode == 'q')
+		{
 			break;
+		}
+			
 	}
 
 	cam->Close();
 
 
 	return 0;
+}
+
+
+Mat Threshold_Demo(Mat input)
+{
+	
+	/* 0: Binary
+	   1: Binary Inverted
+	   2: Threshold Truncated
+	   3: Threshold to Zero
+	   4: Threshold to Zero Inverted
+	 */
+
+
+
+	cvtColor(input, input, COLOR_BGR2GRAY);
+	blur(input, input, Size(5, 5));
+
+
+	 threshold(input, input, low_thresh, max_thresh1, THRESH_BINARY);
+	 //dilate(image_gray, image_gray, 0);
+	namedWindow(thresh_window, WINDOW_AUTOSIZE);
+	createTrackbar("Low H", thresh_window, &low_thresh, max_thresh1);
+
+	imshow(thresh_window, input);
+	
+	return input;
+
+}
+
+
+const clock_t begin_time = clock();
+float time12;
+float time13;
+
+
+void checkForHand(Mat frame) {
+
+	// do something
+	time12 = float(clock() - begin_time) / CLOCKS_PER_SEC;
+	float time13 = time12;
+
+	while (true) {
+		
+		
+
+			
+			if (time13 + 5.0 < time12) {
+
+				float time13 = time12;
+
+				//std::cout << float(clock() - begin_time) / CLOCKS_PER_SEC;
+
+				cout << time12 << endl;
+			}
+
+		
+		time12 = float(clock() - begin_time) / CLOCKS_PER_SEC;
+	}
+
+
+
+
+	
+}
+
+Mat thresh_callback(Mat input)
+{
+	Mat canny_output;
+	vector<vector<Point> > contours;
+	vector<Vec4i> hierarchy;
+
+
+	/// Find contours
+	findContours(input, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE, Point(0, 0));
+	//cout << "size of contours"+contours.size() << endl;
+
+	vector< vector<Point> > hull(contours.size());
+
+	//
+
+	/// Get the moments
+	vector<Moments> mu(contours.size());
+	for (int i = 0; i < contours.size(); i++)
+	{
+		mu[i] = moments(contours[i], false);
+	}
+
+	///  Get the mass centers:
+	vector<Point2f> mc(contours.size());
+	for (int i = 0; i < contours.size(); i++)
+	{
+		mc[i] = Point2f(mu[i].m10 / mu[i].m00, mu[i].m01 / mu[i].m00);
+	}
+
+	for (int i = 0; i < contours.size(); i++)
+		convexHull(Mat(contours[i]), hull[i], false);
+
+	/// Draw contours
+
+	for (int i = 0; i < contours.size(); i++)
+	{
+
+		Scalar color = Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
+
+
+
+		double area1 = boundingRect(contours[i]).width * boundingRect(contours[i]).height; // are of the blob
+		//cout << contourArea(contours[i], false) << endl;
+		//by area
+		bounding_rect = boundingRect(contours[i]); 
+
+
+		//double size1 = contourArea(contours[i], true);
+		double MinAreaLimit = 600.0;				// blob limits
+		double MaxiAreaLimit = 13000.0;//
+		if (area1 > MinAreaLimit && MaxiAreaLimit < 13000)  {				//&& area1<10000  && boundingRect(contours[i-1]).x != boundingRect(contours[i]).x && boundingRect(contours[i-1]).y != boundingRect(contours[i]).y
+
+			//for (int i = 0; i < contours.size(); i++) {
+			bounding_rect = boundingRect(contours[i]);
+
+			cout << " contour number " << endl;
+			cout << i << endl;
+			//cout << "size of contour Area" << endl;
+			cout << area1 << endl;
+			double x, y;
+
+
+			// center of contour 
+			x = boundingRect(contours[i]).x + boundingRect(contours[i]).width / 2;
+			y = boundingRect(contours[i]).y + boundingRect(contours[i]).height / 2;
+
+			area0 = Point(x, y);// the middle of the bounding rectangle
+
+			// draws rectangle on the bounding rectangle area
+			rectangle(frame_threshold, bounding_rect, Scalar(255, 255, 255), 1, 8, 0);
+						
+
+
+			//region of interest set to the bounding rectangle of the blob
+
+			Mat Roi = camFrame(bounding_rect); 
+			rectangle(camFrame, bounding_rect, Scalar(255, 255, 255), 1, 8, 0);
+
+		const char* source_window = "Source";
+			color1(Roi);
+			namedWindow(source_window);
+			imshow(source_window, Roi);
+			Roi.release();
+
+			// draws a circle in the middle of the blob
+			circle(frame_threshold, area0, 4,color, -1, 8, 0);
+			//string s = to_string(i);
+
+
+			// defines if the blob is vertical or horizontal or square(which is almost never)
+
+			if (boundingRect(contours[i]).width > boundingRect(contours[i]).height) {
+				putText(frame_threshold,
+					"hor",
+					cv::Point(area0), // Coordinates
+					cv::FONT_HERSHEY_COMPLEX_SMALL, // Font
+					1.0, // Scale. 2.0 = 2x bigger
+					cv::Scalar(255, 0, 0), // BGR Color
+					1);
+
+			}
+			else if (boundingRect(contours[i]).width < boundingRect(contours[i]).height) {
+
+				putText(frame_threshold,
+					"ver",
+					cv::Point(area0), // Coordinates
+					cv::FONT_HERSHEY_COMPLEX_SMALL, // Font
+					1.0, // Scale. 2.0 = 2x bigger
+					cv::Scalar(255, 255, 255), // BGR Color
+					1);
+
+
+
+			}
+			else {
+				putText(frame_threshold,
+					"square",
+					cv::Point(area0), // Coordinates
+					cv::FONT_HERSHEY_COMPLEX_SMALL, // Font
+					1.0, // Scale. 2.0 = 2x bigger
+					cv::Scalar(255, 0, 0), // BGR Color
+					1);
+
+
+
+			}
+
+			// Anti-alias (Optional)
+	   //}
+			area1 = 0.0; // resets area of the blob
+
+		}
+		/// Show in a window
+		namedWindow("Contours", WINDOW_AUTOSIZE);
+		imshow("Contours", frame_threshold);
+
+		/// Calculate the area with the moments 00 and compare with the result of the OpenCV function
+		//printf("\t Info: Area and Contour Length \n");
+
+	}
+	return input;
+}
+
+
+
+void color1(Mat image) {
+	Mat image_hsv;
+
+	cvtColor(image, image_hsv, COLOR_BGR2HSV);
+
+	// Quanta Ratio
+	int scale = 10;
+
+	int hbins = 36, sbins = 25, vbins = 25;
+	int histSize[] = { hbins, sbins, vbins };
+
+	float hranges[] = { 0, 180 };
+	float sranges[] = { 0, 256 };
+	float vranges[] = { 0, 256 };
+
+	const float* ranges[] = { hranges, sranges, vranges };
+	MatND hist;
+
+	int channels[] = { 0, 1, 2 };
+
+	calcHist(&image_hsv, 1, channels, Mat(), // do not use mask
+		hist, 3, histSize, ranges,
+		true, // the histogram is uniform
+		false);
+
+	int maxVal = 0;
+
+	int hue = 0;
+	int saturation = 0;
+	int value = 0;
+
+	for (int h = 0; h < hbins; h++)
+		for (int s = 0; s < sbins; s++)
+			for (int v = 0; v < vbins; v++)
+			{
+				int binVal = hist.at<int>(h, s, v);
+				if (binVal > maxVal)
+				{
+					maxVal = binVal;
+
+					hue = h;
+					saturation = s;
+					value = v;
+				}
+			}
+
+	hue = hue * scale; // angle 0 - 360
+	saturation = saturation * scale; // 0 - 255
+	value = value * scale; // 0 - 255
+
+	if (hue < 155 && hue >135 && saturation > 115 && saturation < 135) {
+
+		putText(frame_threshold,
+			"Green",
+			cv::Point(area0), // Coordinates
+			cv::FONT_HERSHEY_COMPLEX_SMALL, // Font
+			1.0, // Scale. 2.0 = 2x bigger
+			cv::Scalar(255, 255, 255), // BGR Color
+			1);
+
+
+	}
+	else if (hue < 230 && hue >210 && saturation > 150 && saturation < 175) {
+
+
+		putText(frame_threshold,
+			"Blue",
+			cv::Point(area0), // Coordinates
+			cv::FONT_HERSHEY_COMPLEX_SMALL, // Font
+			1.0, // Scale. 2.0 = 2x bigger
+			cv::Scalar(255, 255, 255), // BGR Color
+			1);
+
+
+	}
+
+
+
+	
+	if (hue < 50 && hue > 20) {
+		switchColor = 0;
+
+	}
+	else if (hue < 50 && hue > 20) {
+
+		switchColor = 1;
+
+	}
+	else if (hue < 50 && hue > 20) {
+
+		switchColor = 2;
+
+	}
+	
+		//Sets the values of the colors
+	switch (switchColor) {
+
+	case 0:
+		maxHueRed = hue + margin;
+		minHueRed = hue - margin;
+		maxSatRed = saturation + margin;
+		minSatRed = saturation - margin;
+		maxValueRed = value + margin;
+		minValueRed = value - margin;
+
+		cout << "color is red" << endl;
+		colorFound = false;
+		break;
+
+	case 1:
+		maxHueGreen = hue + margin;
+		minHueGreen = hue - margin;
+		maxSatGreen = saturation + margin;
+		minSatGreen = saturation - margin;
+		maxValueGreen = value + margin;
+		minValueGreen = value - margin;
+
+		cout << "color is green" << endl;
+		colorFound = false;
+		break;
+
+	case 2:
+		maxHueBlue = hue + margin;
+		minHueBlue = hue - margin;
+		maxSatBlue = saturation + margin;
+		minSatBlue = saturation - margin;
+		maxValueBlue = value + margin;
+		minValueBlue = value - margin;
+
+		cout << "colort is blue" << endl;
+		colorFound = false;
+		break;
+
+	default:
+		cout << "color not found" << endl;
+		colorFound = false;
+
+		break;
+
+
+	}
+
+
+
+
+	cout << hue << "hue" << endl;
+	cout << saturation << "saturation" << endl;
+	cout << value << "value" << endl;
+}
+
+
+
+Mat colorExtraction(Mat input) {
+
+	
+	 cvtColor(input, input, COLOR_BGR2HSV);
+
+	inRange(input,
+        Scalar(minHue, 0, 0),
+        Scalar(maxHue, maxSat, maxValue),
+        input);
+
+
+
+
+	return input;
 }
--- Project2/Source.cpp
+++ Project2/Source.cpp
@@ -4,13 +4,21 @@
 #include "opencv2/imgproc.hpp"
 #include "opencv2/videoio.hpp"
 #include <iostream>
+#include <string> 
+#include <ctime>
 
 //http://raphael.candelier.fr/?blog=Image%20Moments object orientation
 
 
 using namespace std;
 using namespace cv;
-int thresh = 100;
+
+Mat GridCheckingsAndSuch(Mat dispImage_mat);
+Mat CameraGrid(Mat grid_mat);
+Mat SquareGrid(Mat grid_mat);
+
+
+int thresh = 255;
 int max_thresh = 255;
 RNG rng(12345);
 Mat image, image_gray;
@@ -23,6 +31,17 @@ Rect bounding_rect;
  int max_thresh1 = 255;
 
 
+ //dialation 
+
+ Mat src, erosion_dst, dilation_dst;
+ int erosion_elem = 0;
+ int erosion_size = 0;
+ int dilation_elem = 0;
+ int dilation_size = 0;
+ int const max_elem = 2;
+ int const max_kernel_size = 21;
+ void Erosion(int, void*);
+ void Dilation(int, void*);
 
 void thresh_callback(int, void*);
 void Threshold_Demo(int, void*);
@@ -34,27 +53,74 @@ int main(int argc, char** argv)
 
 
 
-	image = imread("Image.png", IMREAD_COLOR);
+	image = imread("Image6.png", IMREAD_COLOR);
 
 	if (!image.data)  // Check for invalid input
 	{
 		cout << "Could not open or find the image" << std::endl;
 		return -1;
 	}
+
+
+
+
+
+
+
 	
+
+	namedWindow("Erosion Demo", WINDOW_AUTOSIZE);
+	namedWindow("Dilation Demo", WINDOW_AUTOSIZE);
+	moveWindow("Dilation Demo", image.cols, 0);
+
+
+
+	namedWindow("Erosion Demo", WINDOW_AUTOSIZE);
+	namedWindow("Dilation Demo", WINDOW_AUTOSIZE);
+	moveWindow("Dilation Demo", image.cols, 0);
+	createTrackbar("Element:\n 0: Rect \n 1: Cross \n 2: Ellipse", "Erosion Demo",
+		&erosion_elem, max_elem,
+		Erosion);
+	createTrackbar("Kernel size:\n 2n +1", "Erosion Demo",
+		&erosion_size, max_kernel_size,
+		Erosion);
+	createTrackbar("Element:\n 0: Rect \n 1: Cross \n 2: Ellipse", "Dilation Demo",
+		&dilation_elem, max_elem,
+		Dilation);
+	createTrackbar("Kernel size:\n 2n +1", "Dilation Demo",
+		&dilation_size, max_kernel_size,
+		Dilation);
+
 	
 
 	cvtColor(image, image_gray, COLOR_BGR2GRAY);
-	blur(image_gray, image_gray, Size(3, 3));
+
+	//bilateralFilter(image_gray, image_gray,3, 75, 75);
+	GaussianBlur(image_gray, image_gray, Size(5, 5), 0, 0);
+	
 	const char* source_window = "Source";
 	namedWindow(source_window);
-	imshow(source_window, image);
-	erode(image_gray,image_gray,2);
-	//xThreshold_Demo(0, 0);
+	//dilate(image_gray, image_gray, 3);
+	
 
+	//xThreshold_Demo(0, 0);
+	//Mat cameraGrid = CameraGrid(image_gray);
+
+	Mat squareGrid = SquareGrid(image_gray);
+
+
+	namedWindow("Contours", WINDOW_AUTOSIZE);
+	imshow("Contours", image_gray);
 
 	createTrackbar("Canny thresh:", source_window, &thresh, max_thresh, thresh_callback);
-	thresh_callback(0, 0);
+
+
+	Erosion(0, 0);
+	Dilation(0, 0);
+	
+
+
+	//thresh_callback(0, 0);
 	
 		waitKey(0); // Wait for a keystroke in the window
 		return 0;
@@ -75,7 +141,7 @@ void Threshold_Demo(int, void*)
 	createTrackbar("Low H", thresh_window, &low_thresh, max_thresh1, Threshold_Demo);
 	
 	imshow(thresh_window, image_gray);
-	
+
 }
 
 
@@ -89,10 +155,14 @@ void thresh_callback(int, void*)
 	vector<vector<Point> > contours;
 	vector<Vec4i> hierarchy;
 
+
 	/// Detect edges using canny
 	Canny(image_gray, canny_output, thresh, thresh * 2, 3);
 	/// Find contours
-	findContours(canny_output, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE, Point(0, 0));
+	findContours(canny_output, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE, Point(0, 0));
+	//cout << "size of contours"+contours.size() << endl;
+
+
 
 	//bb = contours.size();
 	vector< vector<Point> > hull(contours.size());
@@ -131,12 +201,8 @@ void thresh_callback(int, void*)
 	for (int i = 0; i < contours.size(); i++) {
 		totalX += mc[i].x;
 		totalY += mc[i].y;
-		
-		
-
-
-		
 	}
+	
 
 
 	Mat drawing = Mat::zeros(canny_output.size(), CV_8UC3);
@@ -171,27 +237,112 @@ void thresh_callback(int, void*)
 	
 	for (int i = 0; i < contours.size(); i++)
 	{
-		Scalar color = Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
-		double a = contourArea(contours[i], false);
-		cout << contourArea(contours[i], false) << endl;
-		//by area
-		if (a > largest_area) {
-			largest_area = a;
-			largest_contour_index = i;    //Store the index of largest contour
-			
-			
-			bounding_rect = boundingRect(contours[i]); // Find the bounding rectangle for biggest contour
-		}
-		if (contourArea(contours[i], false) > 10) {
 
-			drawContours(drawing, hull, i, color, 1, 8, vector<Vec4i>(), 0, Point());
+		Scalar color = Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
+		
+		
+		
+		double area1 = boundingRect(contours[i]).width * boundingRect(contours[i]).height;
+		//cout << contourArea(contours[i], false) << endl;
+		//by area
+		bounding_rect = boundingRect(contours[i]);
+
+		
+
+
+
+
+		//double size1 = contourArea(contours[i], true);
+		double limit = 600.0;
+		if (area1>limit ){				//  && boundingRect(contours[i-1]).x != boundingRect(contours[i]).x && boundingRect(contours[i-1]).y != boundingRect(contours[i]).y
+
+			//for (int i = 0; i < contours.size(); i++) {
+				bounding_rect = boundingRect(contours[i]);
+				
+				cout << " contour number " << endl;
+				cout << i << endl;
+				cout << "size of contour Area" << endl;
+				cout << area1 << endl;
+				double x, y;
+			
+					x = boundingRect(contours[i]).x + boundingRect(contours[i]).width / 2;
+					y = boundingRect(contours[i]).y + boundingRect(contours[i]).height / 2;
+
+				
+				
+
+				Point area0 = Point(x, y);
+
+
+
+				rectangle(drawing, bounding_rect, Scalar(0, 255, 0), 1, 8, 0);
+				circle(drawing, area0, 4, color, -1, 8, 0);
+				string s = to_string(i);
+
+				//	putText(drawing,
+				//	s,
+				//	cv::Point(area0), // Coordinates
+				//	cv::FONT_HERSHEY_COMPLEX_SMALL, // Font
+				//	1.0, // Scale. 2.0 = 2x bigger
+				//	cv::Scalar(255, 255, 255), // BGR Color
+				//	1);
+
+					if (boundingRect(contours[i]).width > boundingRect(contours[i]).height) {
+						putText(drawing,
+							"hor",
+							cv::Point(area0), // Coordinates
+							cv::FONT_HERSHEY_COMPLEX_SMALL, // Font
+							1.0, // Scale. 2.0 = 2x bigger
+							cv::Scalar(255, 255, 255), // BGR Color
+							1);
+
+					}
+					else if (boundingRect(contours[i]).width < boundingRect(contours[i]).height) {
+
+						putText(drawing,
+							"ver",
+							cv::Point(area0), // Coordinates
+							cv::FONT_HERSHEY_COMPLEX_SMALL, // Font
+							1.0, // Scale. 2.0 = 2x bigger
+							cv::Scalar(255, 255, 255), // BGR Color
+							1);
+
+
+
+					}
+					else {
+						putText(drawing,
+							"square",
+							cv::Point(area0), // Coordinates
+							cv::FONT_HERSHEY_COMPLEX_SMALL, // Font
+							1.0, // Scale. 2.0 = 2x bigger
+							cv::Scalar(255, 255, 255), // BGR Color
+							1);
+
+
+
+					}
+
+				 // Anti-alias (Optional)
+			//}
+				area1 = 0.0;
+
+		}
+
+
+
+		
+
+
+		//bounding_rect = boundingRect(contours[i]);
+			//drawContours(drawing, hull, i, color, 1, 8, vector<Vec4i>(), 0, Point());
 			//drawContours(drawing, contours, i, color, 2, 8, hierarchy, CHAIN_APPROX_SIMPLE, Point());
-			rectangle(drawing, bounding_rect, Scalar(0, 255, 0), 1, 8, 0);
+			
 
 			// draw a line between 2 center points
-			line(drawing, Point(0, 0), Point(mc[4].x, mc[4].y), Scalar(110, 220, 0), 2, 8);
-			circle(drawing, mc[i], 4, color, -1, 8, 0);
-		}
+			//line(drawing, Point(0, 0), Point(mc[4].x, mc[4].y), Scalar(110, 220, 0), 2, 8);
+			
+		//}
 	}
 	
 	//Mat gray, thresh;
@@ -223,13 +374,71 @@ void thresh_callback(int, void*)
 
 	/// Calculate the area with the moments 00 and compare with the result of the OpenCV function
 	//printf("\t Info: Area and Contour Length \n");
-	for (int i = 0; i < contours.size(); i++)
-	{
-		//printf(" * Contour[%d] - Area (M_00) = %.2f - Area OpenCV: %.2f - Length: %.2f \n", i, mu[i].m00, contourArea(contours[i]), arcLength(contours[i], true));
-		Scalar color = Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
-		drawContours(drawing, contours, i, color, 2, 8, hierarchy, 0, Point());
-		circle(drawing, mc[i], 4, color, -1, 8, 0);
-	}
+	
 
 
+}
+
+void Erosion(int, void*)
+{
+	int erosion_type = 0;
+	if (erosion_elem == 0) { erosion_type = MORPH_RECT; }
+	else if (erosion_elem == 1) { erosion_type = MORPH_CROSS; }
+	else if (erosion_elem == 2) { erosion_type = MORPH_ELLIPSE; }
+	Mat element = getStructuringElement(erosion_type,
+		Size(2 * erosion_size + 1, 2 * erosion_size + 1),
+		Point(erosion_size, erosion_size));
+	erode(image_gray, image_gray, element);
+	imshow("Erosion Demo", image_gray);
+}
+void Dilation(int, void*)
+{
+	int dilation_type = 0;
+	if (dilation_elem == 0) { dilation_type = MORPH_RECT; }
+	else if (dilation_elem == 1) { dilation_type = MORPH_CROSS; }
+	else if (dilation_elem == 2) { dilation_type = MORPH_ELLIPSE; }
+	Mat element = getStructuringElement(dilation_type,
+		Size(2 * dilation_size + 1, 2 * dilation_size + 1),
+		Point(dilation_size, dilation_size));
+	dilate(image_gray, image_gray, element);
+	imshow("Dilation Demo", image_gray);
+}
+
+int gridSize_i = 40;
+Mat CameraGrid(Mat gridinput_mat)
+{
+	int width_i = gridinput_mat.rows;  // the width of the grid matches the rows(all pixels in horizontal direction )
+	int height_i = gridinput_mat.cols;
+	int stepSize_i = 24; // calculating how many squares the grid will have 
+
+	 cout << stepSize_i << endl;
+
+	for (int i = 0; i <= width_i; i++) // drawing horizontal lines 
+		line(gridinput_mat, Point(6*stepSize_i, i * stepSize_i), Point(gridSize_i * stepSize_i, i * stepSize_i), Scalar(255, 255, 255));
+
+	for (int i = 0; i <= height_i; i++)// drawing vertical lines 
+		line(gridinput_mat, Point(i * stepSize_i, 6*stepSize_i), Point(i * stepSize_i, gridSize_i * stepSize_i), Scalar(255, 255, 255));
+
+	return gridinput_mat;
+}
+
+Mat SquareGrid(Mat gridinput_mat)
+{
+	int width_i = gridinput_mat.rows;  // the width of the grid matches the rows(all pixels in horizontal direction )
+	int height_i = gridinput_mat.cols;
+	int stepSize_i = 24; // calculating how many squares the grid will have 
+
+	cout << stepSize_i << endl;
+
+	for (int i = 0; i <= width_i; i++) // drawing horizontal lines 
+
+		rectangle(gridinput_mat, Point(6 * stepSize_i, i * stepSize_i), Point(gridSize_i * stepSize_i, i * stepSize_i), Scalar(255, 255, 255));
+
+
+		//line(gridinput_mat, Point(6 * stepSize_i, i * stepSize_i), Point(gridSize_i * stepSize_i, i * stepSize_i), Scalar(255, 255, 255));
+
+	for (int i = 0; i <= height_i; i++)// drawing vertical lines 
+		//line(gridinput_mat, Point(i * stepSize_i, 6 * stepSize_i), Point(i * stepSize_i, gridSize_i * stepSize_i), Scalar(255, 255, 255));
+
+	return gridinput_mat;
 }
